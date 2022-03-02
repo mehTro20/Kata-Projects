cf3eb9debd02a48d2f035b0ddffce9d6c1f157bf
mehTro20 <justmondlisok@gmail.com>
2022-03-02 13:32:22 +0200

    kata1

---
+++ script.js
@@ -0,0 +1,8 @@
+function nameReturn(name) {
+    if (name === "" || name === undefined)
+        return "Hello, World";
+ else {
+      return "Hello, " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+ }
+}
+console.log(nameReturn("muaNO"))
